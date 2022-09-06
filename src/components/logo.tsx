13661af161ece59c7fca7e0eeb34cc08ffa142cd
JakubCisowski <jakub.cisowski1@gmail.com>
2022-09-06 17:07:09 +0200

    Remove demo html/css. Add website logo and icon.

---
+++ src/components/logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className="logo-container">
+      <h1>Town of Salem</h1>
+      <h2>Ineractive notes</h2>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import "../styles/globals.css";
-import type { AppType } from "next/dist/shared/lib/utils";
+import type { AppType } from 'next/dist/shared/lib/utils';
+import Head from 'next/head';
+import Logo from '../components/logo';
+import '../styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Notes of Salem</title>
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <div className="body-container">
+        <Logo />
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 };
 
 export default MyApp;
